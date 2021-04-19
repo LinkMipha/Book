253557522c89c9725a84c94e77c2fee8a03acbed
孙培宏 <sunpeihong@songguo7.com>
2021-04-19 21:05:09 +0800

    增加接口

--- gin_server/controller.go
+++ gin_server/controller.go
@@ -22,6 +22,19 @@ type HelloRsp struct {
 	}
 }
 
+
+
+type CheckOutRsp struct {
+	Status string `json:"status"`
+	Description string `json:"description"`
+	Id string `json:"_id"`
+	UpdateTime  string `json:"updatedAt"`
+	CreatedAt  string `json:"createdAt"`
+	UserName string `json:"userName"`
+	Name string `json:"name"`
+	IsAdmin bool `json:"isAdmin"`
+}
+
 func GetTest(c*gin.Context)  {
 	req:= HelloReq{}
 	rsp:= HelloRsp{}
@@ -34,16 +47,21 @@ func GetTest(c*gin.Context)  {
 	c.JSON(200,rsp)
 }
 
-func GetMenu(c*gin.Context)  {
+func GetMenu(c *gin.Context) {
 
-	c.JSON(200,"")
+	c.JSON(200, "")
 }
 
-func LoginIn(c *gin.Context)  {
-	c.JSON(200,"")
+func LoginIn(c *gin.Context) {
+	c.JSON(200, "")
 }
 
-
-func CheckOut(c *gin.Context)  {
-	c.JSON(200,"")
+func CheckOut(c *gin.Context) {
+	rsp := CheckOutRsp{}
+	rsp.IsAdmin = true
+	rsp.Name = "管理员"
+	rsp.CreatedAt = "2021-04-07T02:51:16.008Z"
+	rsp.UpdateTime = "2021-04-07T02:51:16.008Z"
+	rsp.Id = "606d1e24eebc850a6c30d1c1"
+	c.JSON(200, rsp)
 }
